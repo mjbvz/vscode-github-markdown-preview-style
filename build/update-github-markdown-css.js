2395fd88ef452e24fdd7662f4dc9d30f79803d74
Borek Bernard <borekb@gmail.com>
2019-04-04 23:38:57 +0200

    Allow font size configuration
    - The build script was updated to comment out the explicit `font-size: 16px;` for `.vscode-body`.
    
    - CSS was regenerated.

--- build/update-github-markdown-css.js
+++ build/update-github-markdown-css.js
@@ -9,11 +9,23 @@ const fullPath = path.join(__dirname, '..', inputPath);
 
 function updateGithubMarkdownCss(input) {
     return `/* Generated from '${inputPath}' */\n`+ input.replace(/\.markdown-body/g, '.vscode-body');
-} 
+}
+
+/**
+ * Comments out `font-size: 16px;` so that the `markdown.preview.fontSize` configuration applies.
+ */
+function enablePreviewFontSize(input) {
+    return input.replace(
+        // https://regex101.com/r/LL2lma/2
+        /(\.vscode-body {[^}]+)(font-size: \d+px;)([^}]+})/s,
+        '$1/* $2 */   /* let\'s inherit `markdown.preview.fontSize` */$3'
+    );
+}
 
 
 const input = fs.readFileSync(fullPath, 'utf8');
 
 fs.writeFileSync(
     path.join(__dirname, '..', 'github-markdown.css'),
-    updateGithubMarkdownCss(input));
+    enablePreviewFontSize(updateGithubMarkdownCss(input))
+);
