5e2a265eeedf8225335f8e902f0d44a5d0d58187
SNDST00M: M.U.N.I.N <82655227+SNDST00M@users.noreply.github.com>
2021-10-17 17:34:05 +0100

    Add dark theme support

--- build/update-github-markdown-css.js
+++ build/update-github-markdown-css.js
@@ -3,17 +3,31 @@
 var path = require('path');
 var fs = require('fs');
 
-const inputPath = path.join('node_modules', 'github-markdown-css', 'github-markdown.css');
-const fullPath = path.join(__dirname, '..', inputPath);
+const GITHUB_THEME_DARK = 'dark';
+const GITHUB_THEME_LIGHT = 'light';
+const themes = [GITHUB_THEME_LIGHT, GITHUB_THEME_DARK];
 
+function updateGithubMarkdownCss(themes) {
+    return themes
+        .map(function(theme) {
+            const data = {};
+            data.theme = theme;
+            data.input = path.join('node_modules', 'github-markdown-css', `github-markdown-${theme}.css`);
+            data.path = path.join(__dirname, '..', data.input);
+            return data;
+        })
+        .map(generateGithubMarkdownThemeCss)
+        .join('\n\n');
+}
 
-function updateGithubMarkdownCss(input) {
-    return `/* Generated from '${inputPath}' */\n`+ input.replace(/\.markdown-body/g, '.vscode-body');
-} 
-
-
-const input = fs.readFileSync(fullPath, 'utf8');
+function generateGithubMarkdownThemeCss(data) {
+    const input = fs.readFileSync(data.path, 'utf8').trim()
+        .split('.markdown-body')
+        .join(data.theme === GITHUB_THEME_DARK ? '.vscode-body.vscode-dark' : '.vscode-body');
+    return `/* Generated from '${data.input}' */\n` + input;
+}
 
 fs.writeFileSync(
     path.join(__dirname, '..', 'github-markdown.css'),
-    updateGithubMarkdownCss(input));
+    updateGithubMarkdownCss(themes)
+);
